cf8377fa1e98f4b28e02d9fa510c72b9e3a60756
deniel <denielmendoza13@gmail.com>
2025-05-05 13:10:47 +0800

    mo largo na sa products ig human login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/dashboard');
+            return redirect()->intended('/products')->with('success', 'Login successful.');
         }
 
         return back()->withErrors([
